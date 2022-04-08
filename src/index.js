b69bc5c85250d579fbd526cb9b520dc0f927b13b
Mateus Neves Sopranzi <mateussopranzi@gmail.com>
2022-04-08 12:25:04 -0300

    Atividade 01 de React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+import imagem1 from "./images/imagem1.jpg"
 
+const App = () => {
+    const bloco1 = () => ({margin: 'auto', width: 800, backgroundColor: '#dcdcdc', padding: 12, borderRadius: 8 })
+    const textoDoRotulo = "Profissionais de saúde"
+    const nome1 = ({nome: 'José da Silva'})
+    const nome2 = ({nome: 'Maria da Silva'})
+    const nome3 = ({nome: 'Jaqueline Mendes'})
+
+  return(
+    <div style={bloco1()}>
+        <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom: 4, textAlign: 'center'}}>
+        <b>{textoDoRotulo}</b>
+        </label>
+    <div style={{margin: 'auto', width: 700, backgroundColor: '#e4e4e4', padding: 12, borderRadius: 8 }}> 
+       
+        <div className='bloco2'>    
+            <div className="um">
+            <img src={imagem1} alt=""/>
+            <label className='medico1'>
+            {nome1.nome}  
+            </label>    
+            </div>
+            <div className='dois'>
+            <img src={process.env.PUBLIC_URL + "/imagem2.jpg"}/>
+                {nome2.nome}</div>
+            <div className='tres'>
+            <img src="https://media.istockphoto.com/photos/portrait-of-smiling-female-doctor-wearing-uniform-standing-picture-id1346711310?k=20&m=1346711310&s=612x612&w=0&h=axAb3twIL5WonBso0LWk9bitR7P5gEfxJ6g4_tbjXGo=" alt=""/>
+                {nome3.nome}</div>
+        </div>   
+    </div>
+ </div>      
+  )}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
